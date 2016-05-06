731a76a5fc2e50e1d1ac79ad6925bf2b2c6bb063
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2016-05-06 12:01:07 -0500

    On hold: dimension-independent code cannot be made to work with Gmaps

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 #include <CGAL/Generalized_map.h>
 #include <CGAL/Cell_attribute.h>
 
-struct GMap_items {
+struct GMap_items_3 {
   template <class Map>
   struct Dart_wrapper {
     
@@ -27,11 +27,41 @@ struct GMap_items {
     typedef CGAL::GMap_dart<3, Map> Dart;
     typedef CGAL::Cell_attribute<Map, Embedded_attribute_with_id> Attribute_with_point;
     typedef CGAL::Cell_attribute<Map, Attribute_with_id> Attribute;
-    typedef CGAL::cpp0x::tuple<Attribute_with_point,Attribute,Attribute> Attributes;
+    typedef CGAL::cpp11::tuple<Attribute_with_point, Attribute, Attribute, Attribute> Attributes;
   };
 };
 
-typedef CGAL::Generalized_map<3, GMap_items> GMap_3;
+template <unsigned int d>
+struct GMap_items_d {
+  template <class Map>
+  struct Dart_wrapper {
+    
+    struct Attribute_with_id {
+      int id;
+    };
+    
+    struct Embedded_attribute_with_id : Attribute_with_id {
+      CGAL::Cartesian_d<double> point;
+    };
+    
+    typedef CGAL::GMap_dart<d, Map> Dart;
+    typedef CGAL::Cell_attribute<Map, Embedded_attribute_with_id> Attribute_with_point;
+    typedef CGAL::Cell_attribute<Map, Attribute_with_id> Attribute;
+    
+    template <unsigned int attributes_to_add, class Result = CGAL::cpp11::tuple<>>
+    struct Attributes_so_far;
+    
+    template <unsigned int attributes_to_add, class ... Result>
+    struct Attributes_so_far<attributes_to_add, CGAL::cpp11::tuple<Result ...> > {
+      typedef typename Attributes_so_far<attributes_to_add-1, CGAL::cpp11::tuple<Attribute, Result ...> >::tuple tuple;
+    };
+    
+    typedef typename Attributes_so_far<d>::tuple Attributes;
+  };
+};
+
+typedef CGAL::Generalized_map<3, GMap_items_3> GMap_3;
+//typedef CGAL::Generalized_map<3, GMap_items_d<3>> GMap_3;
 
 int main(int argc, const char * argv[]) {
   
